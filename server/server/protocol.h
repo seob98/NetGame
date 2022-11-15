e03fb199ccc46db88318f624e070feae5ce6d154
KMH8359 <gnslgns2000@naver.com>
2022-11-15 10:47:27 +0900

    기획서 수정 및 서버 프레임워크 추가
    <김명훈>
    기획서 오타 수정
    서버 프레임워크 추가

---
+++ server/server/protocol.h
@@ -0,0 +1,57 @@
+#include "Common.h"
+
+#pragma once
+
+#define MAX_ITEM_CNT		15
+#define VERTBLOCKCNT		15
+#define HORZBLOCKCNT		13
+#define BLOCKSIZE		20
+#define INDEX_MAPSTART		30
+#define INDEX_MAPEND		135
+
+enum KEYEVENT {
+	PRESS_LEFT, PRESS_RIGHT,
+	PRESS_UP, PRESS_DOWN, PRESS_SPACE, PRESS_ITEM, PRESS_END
+};
+
+enum STATE { IDLE, UP, DOWN, LEFT, RIGHT, TRAPPED, SAVED, DIE, DEAD, WIN };
+
+enum TEAM { DAO, BAZZI };
+
+struct CLIENTINFO
+{
+	SOCKET sock;
+	unsigned short ID;
+	CPlayer player;
+};
+
+#pragma pack(push, 1)
+
+struct SC_GAMEINFO
+{
+	unsigned short ID;
+	int currentPlayerCnt;
+	int gameStart;
+	TEAM teamId;
+
+	int blockType[MAX_ITEM_CNT];
+	int itemType[MAX_ITEM_CNT];
+};
+
+struct SC_PLAYERUPDATE
+{
+	unsigned short ID;
+	POINT pt;
+	STATE state;
+	int   ballonLength;
+	bool keydown;
+};
+
+struct CS_EVENT
+{
+	unsigned short ID;
+	bool keyType[PRESS_END];
+	unsigned short Index;
+};
+
+#pragma pack(pop)
---
+++ server/server/server.cpp
@@ -0,0 +1,155 @@
+#include "protocol.h"
+
+#define SERVERPORT 9000
+#define BUFSIZE    4096
+#define MAX_PLAYER 4
+
+CLIENTINFO players[MAX_PLAYER];
+SC_GAMEINFO player_data[MAX_PLAYER];
+
+int cur_player = 0;
+
+// 클라이언트와 데이터 통신
+DWORD WINAPI RecvThread(LPVOID arg)
+{
+	int retval;
+	SOCKET client_sock = (SOCKET)arg;
+	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
+	int addrlen;
+	int len;
+	char buf[BUFSIZE + 1];
+
+	// 클라이언트 정보 얻기
+	addrlen = sizeof(clientaddr);
+	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+	
+	// 클라이언트 초기 정보 보내주기
+	SC_GAMEINFO p_data;
+	p_data.ID = cur_player;
+	cur_player++;
+	p_data.gameStart = 0;
+	retval = send(players[cur_player].sock, (char*)&p_data, sizeof(SC_GAMEINFO), 0);
+	if (retval == SOCKET_ERROR) {
+		err_display("send()");
+		return NULL;
+	}
+	while (1) {
+		retval = recv(players[p_data.ID].sock, buf, sizeof(CS_EVENT), MSG_WAITALL);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+		switch (KEY_EVENT) {
+		case PRESS_LEFT:
+			SC_PLAYERUPDATE p_update;
+			p_update.ID = players[p_data.ID].ID;
+			p_update.pt.x = 
+			break;
+		case PRESS_RIGHT:
+			break;
+		case PRESS_UP:
+			break;
+		case PRESS_DOWN:
+			break;
+		case PRESS_SPACE:
+			break;
+		case PRESS_ITEM:
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI UpdateThread(LPVOID arg)
+{
+	int retval;
+	SOCKET client_sock = (SOCKET)arg;
+	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
+	int addrlen;
+	int len;
+	char buf[BUFSIZE + 1];
+
+	// 클라이언트 정보 얻기
+	addrlen = sizeof(clientaddr);
+	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+
+#pragma region 서버객체 업데이트
+	while (1) {
+
+	}
+#pragma endregion
+
+#pragma region 업데이트 내용 클라이언트에 공유
+
+
+#pragma endregion
+
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (1) {
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+			addr, ntohs(clientaddr.sin_port));
+
+		// 스레드 생성
+		hThread = CreateThread(NULL, 0, RecvThread,
+			(LPVOID)client_sock, 0, NULL);
+		if (hThread == NULL) { closesocket(client_sock); }
+		else { CloseHandle(hThread); }
+	}
+
+	// 소켓 닫기
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
